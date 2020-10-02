197afa49b329892232611b67e0c263af30002ef5
dhunganaganesh8 <dhunganaganesh8@gmail.com>
2020-10-02 22:55:13 +0545

    no_image url from cloudinary

--- account/signals.py
+++ account/signals.py
@@ -7,7 +7,7 @@ from actions.utils import create_action
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance, photo="users/no_image.png")
+        Profile.objects.create(user=instance, photo="https://res.cloudinary.com/hc84zgtcm/image/upload/v1601657803/media/users/no_image_ugzyyo.png")
         create_action(instance, 'has created an account')
 
 @receiver(post_save, sender=User)

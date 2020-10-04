cfe47f2229286d82601d82aa027ce3d969b6e59b
dhunganaganesh8 <dhunganaganesh8@gmail.com>
2020-10-04 20:16:22 +0545

    deploy check

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = '/static/'
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
